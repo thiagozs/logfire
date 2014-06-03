3f51f6ea93022bbefaa817e1a4e688f9dde6b15c
Sascha Gehlich <sascha@gehlich.us>
2014-06-03 23:02:28 +0200

    :bug: Fixed lua clean script

--- lib/lua/clean.lua
+++ lib/lua/clean.lua
@@ -1,5 +1,5 @@
 -- Finds all keys with the given prefix and deletes them
 local keys = redis.call('keys', args['prefix'] .. '*')
-for key in pairs(keys) do
+for i, key in pairs(keys) do
   redis.call('del', key)
 end
